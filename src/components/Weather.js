f028d85aa135fb21b856d9acc9f163f4f137de47
Orkun Demirci <orkundemircit147@gmail.com>
2023-12-03 16:37:16 +0300

    feat - update lenghts

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,6 +3,7 @@ import "./Weather.css";
 
 function Weather(){
     const {weather, loadingWeather, tempUnit} = useMainContext();
+    console.log(weather);
     return (
         <>
             <div id="WeatherDays" className="Row Center">
@@ -12,7 +13,8 @@ function Weather(){
                         <div>{new Date(f.date).toLocaleDateString('en-EN', {weekday: 'short'})}</div>
                         <div><img src={f.day.condition.icon} width="64" height="64" ></img></div>
                         <div>{f.day.condition.text}</div>
-                        {tempUnit === "C" ? <div>{f.day.avgtemp_c} &#8451;</div> : <div>{f.day.avgtemp_f} &#8457;</div> }
+                        {tempUnit === "C" ? <div><span>{f.day.maxtemp_c} &#8451;</span> <span>{f.day.mintemp_c} &#8451;</span></div> 
+                        : <div><span>{f.day.maxtemp_f} &#8457;</span> <span>{f.day.mintemp_f} &#8457;</span></div> }
                     </div>
                 ))}
             </div>
